b4fffa75f58b4e5873fb87c37b5fabc5e079e4b3
Shradha Menon <39404261+shradhamenon@users.noreply.github.com>
2019-08-09 14:53:05 +0530

    Update passport-setup.js

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -15,8 +15,8 @@ passport.deserializeUser((id, done) => {
 
 passport.use(new GoogleStrategy({
     callbackURL: 'http://localhost:5000/auth/google/redirect',
-    clientID: '904558563417-8518cksoulutcoftco17pojf86kgiro6.apps.googleusercontent.com',
-    clientSecret: '66q6Ja2s3guzSokzN_si_fdY'
+    clientID: '',
+    clientSecret: ''
 }, (accessToken, refreshToken, profile, done) => {
     User.findOne({ googleID: profile.id }).then((currentUser) => {
         if (currentUser) {
