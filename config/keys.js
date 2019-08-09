e0e60a529812b1bb8b769cec84154b27c582a599
Shradha Menon <39404261+shradhamenon@users.noreply.github.com>
2019-08-09 14:52:12 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,9 @@
 module.exports = {
     google: {
-        clientID: '904558563417-jd6pp8lm5flg2705c5t2573fc3van3v3.apps.googleusercontent.com',
-        clientSecret: '1ZLB8CrxZ-mVvBw8Xy34acSN'
+        clientID: '',
+        clientSecret: ''
     },
     session: {
-        cookieKey: 'bazarrecookie'
+        cookieKey: ''
     }
 }
